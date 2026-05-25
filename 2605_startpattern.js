1f4ac36c7146b635bc462186382a99777e48a1c3
Ekansh Rajput <115173416+regression1607@users.noreply.github.com>
2026-05-26 01:25:08 +0530

    Create 2605_startpattern.js

---
+++ 2605_startpattern.js
@@ -0,0 +1,72 @@
+// Star parttern 
+
+// * * * * 
+// * * * * 
+// * * * * 
+// * * * * 
+
+// function starSquare(n){
+//   for(let i =0 ; i<n ; i++){
+//     let row = " ";
+//     for(let j =0 ; j<n ; j++){
+//       row = row+"*";
+//     }
+//     console.log(row);
+//   }
+// }
+
+// starSquare(4);
+
+
+// *
+// **
+// ***
+// ****
+// function star(n){
+//   for(let i =0 ; i<n ; i++){
+//     let row = " ";
+//     for(let j =0 ; j<i+1 ; j++){
+//       row = row+"*";
+//     }
+//     console.log(row);
+//   }
+// }
+
+// star(4);
+// 1
+// 12
+// 123
+// 1234
+// 12345
+// function starSquare(n){
+//   for(let i =0 ; i<n ; i++){
+//     let row = "";
+//     for(let j =0 ; j<i+1 ; j++){
+//       row = row+(j+1);
+//     }
+//     console.log(row);
+//   }
+// }
+
+// starSquare(5);
+// 1
+// 22
+// 333
+// 4444
+// 55555
+// function starSquare(n){
+//   for(let i =1 ; i<n ; i++){
+//     let row = "";
+//     for(let j =1 ; j<i+1 ; j++){
+//       row = row+(i);
+//     }
+//     console.log(row);
+//   }
+// }
+
+// starSquare(5);
+
+
+
+
+
