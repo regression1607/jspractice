05f01158babddd2f3fdef9827797be0e9200cb49
Ekansh Rajput <115173416+regression1607@users.noreply.github.com>
2026-06-03 01:08:16 +0530

    Update 030626_sum of arrray using recursion.js

--- 030626_sum of arrray using recursion.js
+++ 030626_sum of arrray using recursion.js
@@ -12,3 +12,24 @@ function sum(n){
   return arr[n]+sum(n-1);
 }
 console.log("sum",sum(arr.length-1));
+
+
+// sum of  all odd element in arrray using recursion
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+let arr = [5,4,3,2,0,3];
+function sum(n){
+  let isodd =arr[n]%2!=0;
+  if(n==0){
+    return isodd?arr[n]:0;
+  }
+  return (isodd?arr[n]:0) + sum(n-1);
+}
+console.log("sum",sum(arr.length-1));
+
+
+  
+     
