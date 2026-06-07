31baefad1b275d64ea2b84c2272cfd3f43e4d23f
Ekansh Rajput <115173416+regression1607@users.noreply.github.com>
2026-06-08 02:01:04 +0530

    Create 080626_removedublicate.js

---
+++ 080626_removedublicate.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} val
+ * @return {ListNode}
+ */
+
+var removeElements = function(head, val) {
+    let sentialnode = new ListNode();
+    sentialnode.next=head;
+    let prev=sentialnode;
+    
+    while(prev && prev.next){
+        if(prev.next.val==val){
+            prev.next=prev.next.next;
+        }else{
+            prev=prev.next;
+        }  
+    }
+    return sentialnode.next;
+};
