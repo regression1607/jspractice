f1c0853c5e495722133b7978d9f843d63952ebac
Ekansh Rajput <115173416+regression1607@users.noreply.github.com>
2026-06-09 04:02:30 +0530

    Create 090626_oddevenlinkedlist.js

---
+++ 090626_oddevenlinkedlist.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var oddEvenList = function(head) {
+    if(head==null || head.next===null){
+        return head;
+    }
+    let odd = head;
+    let even = head.next;
+    let secondnode=even;
+    while(even && odd && even.next && odd.next){
+        odd.next=odd.next.next;
+        even.next=even.next.next;
+        odd=odd.next;
+        even=even.next;
+    }
+    odd.next=secondnode;
+    return head;
+};
