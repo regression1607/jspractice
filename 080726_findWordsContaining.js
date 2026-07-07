8e588a7cb7e5d261668bfc6adcd35e34015e4d12
Ekansh Rajput <115173416+regression1607@users.noreply.github.com>
2026-07-08 02:09:28 +0530

    Create 080726_findWordsContaining.js

---
+++ 080726_findWordsContaining.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string[]} words
+ * @param {character} x
+ * @return {number[]}
+ */
+var findWordsContaining = function(words, x) {
+    let out=[];
+    for(let i=0;i<words.length;i++){
+       for(let j =0;j<words[i].length;j++){
+            if(words[i][j]===x){
+                out.push(i);
+                break;
+            }  
+       }
+    }
+    return out;
+};
